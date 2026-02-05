8bb0a47bd1210dd11e9f1dfc4f5515f80d8d24ec
Rohit Kumar <147975130+Kumarrohit93@users.noreply.github.com>
2026-02-05 15:08:06 +0530

    Add deleteCustomer route to AdminEditCustomerStatusRoute

--- routes/AdminEditCustomerStatusRoute.js
+++ routes/AdminEditCustomerStatusRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {editPage, updatePaymentStatus} = require("../controllers/AdminEditCustomerPayStatus")
+const {editPage, updatePaymentStatus, deleteCustomer} = require("../controllers/AdminEditCustomerPayStatus")
 
 router.get("/customer/:id/edit", editPage)
+router.delete("/customer/:id/delete", deleteCustomer)
 router.post("/customer/:id/payment", updatePaymentStatus)
 
 module.exports = router
